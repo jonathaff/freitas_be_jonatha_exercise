6e9a1daca842a92a9a0677de94019133ff36c868
Jonatha Freitas <jfreitas@impresa.pt>
2023-03-16 20:48:11 +0000

    fix test MembershipsApiTests.shouldFailToCreateRoleMembershipWhenTeamDoesNotExist - throw ResourceNotFoundException when team does not exist

--- src/main/java/com/ecore/roles/service/impl/MembershipsServiceImpl.java
+++ src/main/java/com/ecore/roles/service/impl/MembershipsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecore.roles.service.impl;
 
+import com.ecore.roles.client.model.Team;
 import com.ecore.roles.exception.InvalidArgumentException;
 import com.ecore.roles.exception.ResourceExistsException;
 import com.ecore.roles.exception.ResourceNotFoundException;
@@ -8,12 +9,14 @@ import com.ecore.roles.model.Role;
 import com.ecore.roles.repository.MembershipRepository;
 import com.ecore.roles.repository.RoleRepository;
 import com.ecore.roles.service.MembershipsService;
+import com.ecore.roles.service.TeamsService;
 import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static java.util.Optional.ofNullable;
@@ -24,13 +27,16 @@ public class MembershipsServiceImpl implements MembershipsService {
 
     private final MembershipRepository membershipRepository;
     private final RoleRepository roleRepository;
+    private final TeamsService teamsService;
 
     @Autowired
     public MembershipsServiceImpl(
             MembershipRepository membershipRepository,
-            RoleRepository roleRepository) {
+            RoleRepository roleRepository,
+            TeamsService teamsService) {
         this.membershipRepository = membershipRepository;
         this.roleRepository = roleRepository;
+        this.teamsService = teamsService;
     }
 
     @Override
@@ -38,6 +44,8 @@ public class MembershipsServiceImpl implements MembershipsService {
 
         UUID roleId = ofNullable(m.getRole()).map(Role::getId)
                 .orElseThrow(() -> new InvalidArgumentException(Role.class));
+
+        ofNullable(teamsService.getTeam(m.getTeamId())).orElseThrow(() -> new ResourceNotFoundException(Team.class, m.getTeamId()));
 
         if (membershipRepository.findByUserIdAndTeamId(m.getUserId(), m.getTeamId())
                 .isPresent()) {
